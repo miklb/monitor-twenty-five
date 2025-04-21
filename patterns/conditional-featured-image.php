e18c5aa404186f7f889d79a46f17445bc78c614d
Michael Bishop <howdy@michaelbishop.me>
2025-04-20 21:13:35 -0400

    fix: move conditional outside html

--- patterns/conditional-featured-image.php
+++ patterns/conditional-featured-image.php
@@ -6,10 +6,10 @@
  */
 ?>
 
+<?php if (!get_post_meta(get_the_ID(), 'hide_featured_image', true)) : ?>
 <!-- wp:group -->
 <div class="wp-block-group">
-<?php if (!get_post_meta(get_the_ID(), 'hide_featured_image', true)) : ?>
   <!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"object":{"position":"center center"},"width":"100%"}} /-->
-<?php endif; ?>
 </div>
 <!-- /wp:group -->
+<?php endif; ?>
