fb45ce3f3bcacb4a81924eca4cfdf814044648a7
Michael Bishop <howdy@michaelbishop.me>
2025-05-08 12:44:13 -0400

    new: app caption to featured image on single posts. Now allows for crediting images.

--- patterns/conditional-featured-image.php
+++ patterns/conditional-featured-image.php
@@ -9,7 +9,19 @@
 <?php if (!get_post_meta(get_the_ID(), 'hide_featured_image', true)) : ?>
 <!-- wp:group -->
 <div class="wp-block-group">
-  <!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"object":{"position":"center center"},"width":"100%"}} /-->
+  <!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"object":{"position":"center center"},"width":"100%","spacing":{"margin":{"bottom":"0"}}}} /-->
+  
+  <?php 
+  // Get the attachment ID for the featured image
+  $attachment_id = get_post_thumbnail_id(get_the_ID());
+  // Get the caption from the attachment
+  $caption = wp_get_attachment_caption($attachment_id);
+  
+  if (!empty($caption)) : ?>
+  <!-- wp:paragraph {"align":"right","style":{"typography":{"fontStyle":"italic"},"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}},"fontSize":"small"} -->
+  <p class="has-text-align-right has-small-font-size" style="font-style:italic;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10);"><?php echo esc_html($caption); ?></p>
+  <!-- /wp:paragraph -->
+  <?php endif; ?>
 </div>
 <!-- /wp:group -->
 <?php endif; ?>
